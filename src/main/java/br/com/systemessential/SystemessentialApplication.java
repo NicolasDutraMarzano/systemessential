41688bfbc718012a1f3789282e4c80629d52b3e1
Nicolas Marzano <nicolasdutramarzano@gmail.com>
2019-05-14 09:15:17 -0300

    Projeto criado

---
+++ src/main/java/br/com/systemessential/SystemessentialApplication.java
@@ -0,0 +1,13 @@
+package br.com.systemessential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemessentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemessentialApplication.class, args);
+	}
+
+}
